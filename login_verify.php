553a3bfb26472457f7c1b9bcdd62b2993d84ad43
Andrew <Andrew@192.168.0.117>
2016-07-19 16:24:34 -0700

    update

--- login_verify.php
+++ login_verify.php
@@ -1,5 +1,12 @@
 <?php
+$complex_name = "";
+$complex_email = "";
+$phone_number = "";
+$street_address = "";
+$complex_city = "";
+$complex_zip = "";
 
-echo filter_input(INPUT_POST, "complex_name");
+
+if (isset($complex_name))
 ?>
 
