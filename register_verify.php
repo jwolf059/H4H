cd6f4a2df8af5c0689a5922a96b185cc03df577b
Andrew <Andrew@192.168.0.117>
2016-07-21 16:57:04 -0700

    Register form connected to server through php.

---
+++ register_verify.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "mydb";
+
+
+
+    $complex_name = mysql_real_escape_string(filter_input(INPUT_POST, "complex_name"));
+    $complex_email = mysql_real_escape_string(filter_input(INPUT_POST, "complex_email", FILTER_VALIDATE_EMAIL));
+    $phone_number = mysql_real_escape_string(filter_input(INPUT_POST, "phone_number", FILTER_SANITIZE_SPECIAL_CHARS));
+    $street_address = mysql_real_escape_string(filter_input(INPUT_POST, "street_address"));
+    $complex_city = mysql_real_escape_string(filter_input(INPUT_POST, "complex_city"));
+    $complex_zip = mysql_real_escape_string(filter_input(INPUT_POST, "complex_zip"));
+    $complex_password = mysql_real_escape_string(filter_input(INPUT_POST, "password"));
+
+
+    if (isset($complex_name) and isset($complex_email) and
+            isset($phone_number) and isset($street_address) and
+            isset($complex_city) and isset($complex_zip)) {
+
+// Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+// Insert new Complex information into table.
+        $sql = "INSERT INTO User (id,COMPLEX_NAME, COMPLEX_EMAIL, PHONE_NUMBER,
+                STREET_ADDRESS, CITY, ZIP, DESCRIPTION, PASSWORD)
+                VALUES ('1', '$complex_name', '$complex_email', '$phone_number', '$street_address',"
+                . "'$complex_city', '$complex_zip', 'Default Description', '$complex_password')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    }
+    else {
+        echo "Need data!";
+        
+    }
+    ?>
+
+
+</html>
