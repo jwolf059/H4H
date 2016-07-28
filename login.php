f196eb894e690b1bf6cf3a16a14dc516e87d1c80
Andrew <Andrew@192.168.0.117>
2016-07-28 14:09:15 -0700

    Changing Login 2

--- login.php
+++ login.php
@@ -1,19 +1,62 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 
-<HTML>
-    <HEAD>
-        <TITLE></TITLE>
-
-    </HEAD>
 
     <?php
+    session_start();
+    $error_message = '';
+    
+    if (isset($_POST['submit'])) {
+        
+        if (empty($_POST['login_field']) || empty($_POST['password_field'])) {
+            $error_message = "Invalid Login name or Password";
+        }
+        
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "mydb";
+        
+        $landlord_name = $_POST['login_field'];
+        $landlord_password = $_POST['password_field'];
+        
+        $conn = mysql_connect($servername, $username, $password);
+        
+        // If the connection fails, give error.
+        if (!$conn) {
+            die("Connection Failed: " . mysql_error());
+        }
+        
+        // Security formatting
+        $landlord_name = stripcslashes($landlord_name);
+        $landlord_name = mysql_real_escape_string($landlord_name);
+        $landlord_password = stripcslashes($landlord_password);
+        $landlord_password = mysql_real_escape_string($landlord_password);
+        
+        $selected_db = mysql_select_db($database, $conn);
+        
+        $query_string = sprintf("SELECT * FROM User "
+                . "WHERE COMPLEX_EMAIL = '%s' AND PASSWORD = '%s'",
+                $landlord_name, $landlord_password);
+        
+        $query_for_login =  mysql_query($query_string);
+        
+        // Check to see if only one query was found.
+        $rows = mysql_num_rows($query_for_login);
+        
+        if ($rows == 1) {
+            
+            $_SESSION['login_user'] = $landlord_name;
+            header("location: LandlordPage.php");
+        }
+        else {
+            $error_message = "Invalid Login name or Password";
+        }
+        mysql_close($conn);
+        
+    }
+    
+    
 // Only connects to server after POST method has been sent.
-
+/*
     if (isset($_POST['submit'])) {
 
 
@@ -45,10 +88,10 @@ and open the template in the editor.
         if (!$result) {
             die('Could not query:' . mysql_error());
         }
-        /*while ($row = mysql_fetch_assoc($result)) {
+        while ($row = mysql_fetch_assoc($result)) {
             echo $row['COMPLEX_NAME'];
             echo $row['PASSWORD'];
-        }*/
+        }
 
          if (!mysql_num_rows($result) == 0) {
 
@@ -59,7 +102,5 @@ and open the template in the editor.
           } 
         mysql_close($conn);
     }
+    */
     ?>
-
-
-</HTML>
