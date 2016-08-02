527e0d0561780c12cee4a438b3d63ef4f223c0ac
Andrew <Andrew@192.168.0.117>
2016-08-02 14:32:54 -0700

    Correcting Register password.

--- LandlordPage.php
+++ LandlordPage.php
@@ -2,9 +2,9 @@
 <?php
 include('LandlordSession.php');
 
-//$unit_query_string = sprintf("SELECT * FROM USER where COMPLEX_EMAIL='%s'", $user_validation);
+$unit_query_string = sprintf("SELECT * FROM UNITS where COMPLEX_NAME='%s'", $current_complex);
 
-//$query_for_units = mysqli_query($conn, $unit_query_string);
+$query_for_units = mysqli_query($conn, $unit_query_string);
 ?>
 <!DOCTYPE html>
 <!--
@@ -22,13 +22,20 @@ Use the php tags and an echo call before hand to access the variables.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Landlord Property Management</title>
+   
     </head> 
+    
     <body>
+        <h1> <b>Landlord Property Management</b></h1>
         <div id="profile">
             <b id="welcome">Welcome : <i><?php echo $current_complex; ?></i></b>
-            <b id="logout"><a href="LandlordLogout.php">Log Out</a></b>
-            
+            <p id="UnitTable"> <?php
+                while ($row == mysqli_fetch_assoc($query_for_units)) {
+                         printf("%s, %s", $row['COMPLEX_NAME'], $row['PRICE']);
+                        }?>
+                <b id="logout"><a href="LandlordLogout.php">Log Out</a></b>
+
         </div>
     </body>
 </html>
