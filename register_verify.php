e84747b633bf8dafe9ac4856d0b3f03cfc4d1aad
JeremyWolf <JeremyWolf@192.168.0.113>
2016-08-05 12:16:53 -0700

    Picture

--- register_verify.php
+++ register_verify.php
@@ -15,11 +15,55 @@
     $password = "";
     $dbname = "mydb";
 
+    
+        function imageUpload($input) {
+    //file upload variables 
+    $target_dir = "DatabaseIMG/";
+    $Image_dir = sprintf("%s%s", $target_dir, $_FILES[$input]["name"]);
+    $target_file = $target_dir . basename($_FILES[$input]["name"]);
+    $uploadOK = 1;
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
+        //Verify the Image is a real image
+   // if (isset($POST["submit"])) {
+        $check = getimagesize($_FILES["Image_One"]["TEMP"]);
+        if ($check !== false) {
+            echo "File is an Image - " . $check["mime"] . " .";
+            $uploadOK = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOK = 0;
+        }
+   // }
+        // Check file size
+    if ($_FILES["Image_One"]["size"] > 500000) {
+        echo "Your File is too Large, File size limit is 50mb";
+        $uploadOK = 0;
+    }
+
+        //Check for File Format
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" &&
+            $imageFileType != "gif") {
+        echo "Only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOK = 0;
+    }
+    if ($uploadOK == 0) {
+        echo "File upload unsuccessful";
+    } else {
+        if (move_uploaded_file($_FILES["Image_One"]["tmp_name"], $target_file)) {
+            echo "The file has been uploaded";
+        } else {
+            echo " File upload unsuccessful";
+        }
+    }
+    return $Image_dir;
+    }
+ 
+
+    $Image_One_dir = imageUpload('Image_One');
 
     $complex_name = mysql_real_escape_string(filter_input(INPUT_POST, "complex_name"));
-    $complex_email = mysql_real_escape_string(filter_input(INPUT_POST, "complex_email",
-            FILTER_VALIDATE_EMAIL));
+    $complex_email = mysql_real_escape_string(filter_input(INPUT_POST, "complex_email", FILTER_VALIDATE_EMAIL));
     $phone_number = mysql_real_escape_string(filter_input(INPUT_POST, "phone_number"));
     $street_address = mysql_real_escape_string(filter_input(INPUT_POST, "street_address"));
     $complex_city = mysql_real_escape_string(filter_input(INPUT_POST, "complex_city"));
@@ -37,11 +81,13 @@
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-// Insert new Complex information into table.
+
+        // Insert new Complex information into table.
         $sql = "INSERT INTO User (ID, COMPLEX_NAME, COMPLEX_EMAIL, PHONE_NUMBER,
-                STREET_ADDRESS, CITY, ZIP, DESCRIPTION, PASSWORD)
+                STREET_ADDRESS, CITY, ZIP, DESCRIPTION, PASSWORD, IMG_ONE, IMG_TWO)
                 VALUES (DEFAULT, '$complex_name', '$complex_email', '$phone_number', '$street_address',"
-                . "'$complex_city', '$complex_zip', 'Default Description', '$complex_password')";
+                . "'$complex_city', '$complex_zip', 'Default Description', '$complex_password', "
+                . "'$Image_One_dir', '$Image_two_dir')";
 
         if ($conn->query($sql) === TRUE) {
             header("location: LoginPage.php");
@@ -51,9 +97,7 @@
 
         $conn->close();
     }
-    ?>
-
-
+?>
 </html>
 
 
